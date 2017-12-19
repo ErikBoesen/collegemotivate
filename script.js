109a83a066cf37e9f467316fe90e873f13e25a8f
ErikBoesen <me@erikboesen.com>
2017-12-18 20:33:03 -0500

    Add site detection

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var hosts = JSON.parse(localStorage.hosts);
-
-console.log('Good job focusing!');
-if (hosts.includes(location.hostname)) {
-
-} else {
-}
+required = ['hosts'];
+videos = [
+    'GuM8vTq0jd4',
+    '3ntAEPtqH4o',
+    '_Nq4Z5i7lcs',
+    'jZW4diiXtNI'
+];
+chrome.storage.sync.get(required, function(options) {
+    var included = false;
+    for (i in options.hosts)
+        if (location.hostname.indexOf(options.hosts[i]) >= 0)
+            included = true;
+    if (included) {
+        window.open('https://youtube.com/watch?v=' + videos[Math.floor(Math.random() * videos.length)]);
+    } else {} // Not a blacklisted website
+});
